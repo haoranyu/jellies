c10c9ad16fd21dd225067e4419ae88c66c020996
Haoran Yu <yuhaoran@jisuanke.com>
2019-03-22 17:21:41 +0800

    Add checkbox components. #1

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-checkbox
+    v-model="value"
+    v-bind="trimAttrs($attrs)"
+  >
+    <slot></slot>
+  </el-checkbox>
+</template>
+
+<script>
+export default {
+  name: 'JskCheckbox',
+  inheritAttrs: false,
+  data: function() {
+    return {
+      value: ''
+    };
+  },
+  created: function() {
+    this.value = this.vModel;
+  },
+  props: {
+    vModel: Boolean
+  },
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'checkbox-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  },
+  model: {
+    prop: 'vModel',
+    event: 'change'
+  },
+  watch: {
+    value: function() {
+      this.$emit('change', this.value);
+    },
+    vModel: function() {
+      this.value = this.vModel;
+    }
+  }
+}
+</script>
+
+<style>
+.el-checkbox .el-checkbox__inner::after {
+  left: 3px;
+  top: 0;
+}
+.el-checkbox .el-checkbox__inner::before {
+  top: 4px !important;
+}
+</style>
