aed6c866c8a8bc2d41e6b1d257ca2f492ea79ea0
Haoran Yu <haoran_yu@hotmail.com>
2019-03-30 10:06:26 +0800

    Add time picker and time select components. #11

---
+++ src/components/TimeSelect.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <el-time-select
+      v-model="value"
+      v-bind="trimAttrs($attrs)"
+      v-on:focus="$emit('focus')"
+      v-on:blur="$emit('blur')"
+      :align="timeSelectTextAlign"
+      :picker-options="timeSelectOptions"
+    >
+      <slot>
+      </slot>
+    </el-time-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JskTimeSelect',
+  inheritAttrs: false,
+  data: function() {
+    return {
+      value: ''
+    };
+  },
+  created: function() {
+    this.value = this.vModel;
+  },
+  props: {
+    vModel: String,
+    timeSelectOptions: {
+      type: Object
+    },
+    isIntervalSelection: {
+      type: Boolean,
+      default: false
+    },
+    hasArrowsInPanel: {
+      type: Boolean,
+      default: false
+    },
+    timeSelectTextAlign: {
+      type: String,
+      default: 'left'
+    }
+  },
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'time-select-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  },
+  model: {
+    prop: 'vModel',
+    event: 'change'
+  },
+  watch: {
+    value: function() {
+      this.$emit('change', this.value);
+    },
+    vModel: function() {
+      this.value = this.vModel;
+    }
+  }
+}
+</script>
+<style>
+.time-select-item.selected:not(.disabled) {
+  color: #333333 !important;
+}
+</style>
