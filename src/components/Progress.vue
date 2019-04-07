247824523d1341dc2469aca8cd17d58d7ca3ca3e
Haoran Yu <haoran_yu@hotmail.com>
2019-04-07 16:46:28 +0800

    Add progress component. #16

---
+++ src/components/Progress.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-progress
+    v-bind="trimAttrs($attrs)"
+    :type="isCircular ? 'circle' : 'line'"
+    :color="backgroundColorByType"
+    :text-inside="isValueInside"
+    :show-text="hasValue ? true : false"
+    :width="progressDiameter"
+  >
+    <slot>
+    </slot>
+  </el-progress>
+</template>
+
+<script>
+export default {
+  name: 'JskProgress',
+  inheritAttrs: false,
+  props: {
+    isCircular: {
+      type: Boolean,
+      default: false
+    },
+    progressType: {
+      type: String,
+      default: ''
+    },
+    progressBarColor: {
+      type: String,
+      default: ''
+    },
+    progressDiameter: {
+      type: Number,
+      default: 126
+    },
+    hasValue: {
+      type: Boolean,
+      default: false
+    },
+    isValueInside: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    backgroundColorByType: function() {
+      if (this.progressBarColor !== '') {
+        return this.progressBarColor;
+      }
+      switch (this.progressType) {
+        case 'danger':
+          return '#DD2E1D';
+        case 'warning':
+          return '#FFB020';
+        case 'info':
+          return '#97A5BC';
+        case 'success':
+          return '#41B146';
+        default:
+          return '#41B146';
+      }
+    }
+  },
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'progress-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  }
+}
+</script>
+
+<style>
+.el-progress-bar .el-progress-bar__outer {
+  background: #F2F4F7 !important;
+  box-shadow: inset 0 1px 1px 0 rgba(139, 154, 174, 0.3);
+}
+</style>
