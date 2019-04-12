e22c0b5d41a2ef57ee8caf591c6a1f4b1bcfeff6
Haoran Yu <haoran_yu@hotmail.com>
2019-04-12 09:16:04 +0800

    Add table components. #20

---
+++ src/components/Table.vue
@@ -0,0 +1,19 @@
+<script>
+import { Table } from 'element-ui';
+export default Table
+</script>
+
+<style scoped>
+.el-table {
+  border-top: 2px solid #DDD;
+}
+.el-table::before {
+  height: 2px;
+}
+.el-table.el-table--border {
+  border-top: 1px solid #DDD;
+}
+.el-table.el-table--border::before {
+  height: 1px;
+}
+</style>
