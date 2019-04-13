824d437fa048ba0f73a6d6913372a0ceff951ffa
Haoran Yu <haoran_yu@hotmail.com>
2019-04-13 10:58:37 +0800

    Add breadcrumb component. #22

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-breadcrumb
+    :separator="breadcrumbSeparatorChar"
+    :separator-class="breadcrumbSeparatorIconClass"
+  >
+    <slot>
+    </slot>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'JskBreadcrumb',
+  props: {
+    breadcrumbSeparatorChar: {
+      type: String,
+      default: '/'
+    },
+    breadcrumbSeparatorIconClass: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
