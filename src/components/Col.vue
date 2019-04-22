cc0edf84b46ccb70542b68acab653965f92508d9
Haoran Yu <haoran_yu@hotmail.com>
2019-04-22 15:06:53 +0800

    Col and row and changed to indirect mode. Hotfix.

--- src/components/Col.vue
+++ src/components/Col.vue
@@ -1,5 +1,27 @@
-<script>
-import { Col } from 'element-ui';
+<template>
+  <el-col
+    v-bind="trimAttrs($attrs)"
+  >
+    <slot></slot>
+  </el-col>
+</template>
 
-export default Col
+<script>
+export default {
+  name: 'JskCol',
+  inheritAttrs: false,
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'col-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  }
+}
 </script>
