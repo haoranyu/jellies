700a143436e553aa1177e4b0a0047c9d906040b9
Haoran Yu <yuhaoran@jisuanke.com>
2019-04-22 16:32:28 +0800

    Add collapse component. #27

---
+++ src/components/Collapse.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-collapse
+    v-model="value"
+    v-bind="trimAttrs($attrs)"
+  >
+    <slot></slot>
+  </el-collapse>
+</template>
+
+<script>
+export default {
+  name: 'JskCollapse',
+  inheritAttrs: false,
+  data: function() {
+    return {
+      value: ''
+    };
+  },
+  created: function() {
+    this.value = this.vModel;
+  },
+  props: {
+    vModel: [String, Array],
+    isUnderline: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'collapse-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  },
+  model: {
+    prop: 'vModel',
+    event: 'change'
+  },
+  watch: {
+    value: function() {
+      this.$emit('change', this.value);
+    },
+    vModel: function() {
+      this.value = this.vModel;
+    }
+  }
+}
+</script>
---
+++ src/components/CollapseItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-collapse-item
+    v-bind="trimAttrs($attrs)"
+  >
+    <template slot="title">
+      <slot name="title"></slot>
+    </template>
+    <slot></slot>
+  </el-collapse-item>
+</template>
+
+<script>
+export default {
+  name: 'JskCollapseItem',
+  inheritAttrs: false,
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'collapse-item-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  }
+}
+</script>
