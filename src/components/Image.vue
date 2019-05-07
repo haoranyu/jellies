2b4e9ed9b8ef0f0c2250acb9530fa47a86237ece
Haoran Yu <haoran_yu@hotmail.com>
2019-05-07 11:47:06 +0800

    Add image component. #31

---
+++ src/components/Image.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-image
+    v-bind="trimAttrs($attrs)"
+    :lazy="lazyload === 'on'"
+    :scroll-container="imageContainer"
+    :fit="objectFit"
+    v-on:load="$emit('load')"
+    v-on:error="$emit('error')"
+  >
+    <template slot="placeholder">
+      <slot name="placeholder"></slot>
+    </template>
+    <template slot="error">
+      <slot name="error"></slot>
+    </template>
+  </el-image>
+</template>
+
+<script>
+export default {
+  name: 'JskImage',
+  inheritAttrs: false,
+  props: {
+    lazyload: {
+      type: String,
+      default: 'auto'
+    },
+    objectFit: {
+      type: String,
+      default: 'auto'
+    },
+    imageContainer: {}
+  },
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'image-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  }
+}
+</script>
