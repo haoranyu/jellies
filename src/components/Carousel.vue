a2547684cf57c36c142be5b8c4c1dff25e8a8a3a
Haoran Yu <haoran_yu@hotmail.com>
2019-06-10 19:34:47 +0800

    Add Carousel component. #43

---
+++ src/components/Carousel.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-carousel
+    v-bind="trimAttrs($attrs)"
+    :indicator-position="isOutsideIndicator ? 'outside' : 'none'"
+    :type="isCard ? 'card' : ''"
+    :direction="isVertical ? 'vertical' : 'horizontal'"
+    :arrow="carouselShowArrow"
+    v-on:change="$emit('change')">
+    <slot></slot>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  name: 'JskCarousel',
+  inheritAttrs: false,
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'carousel-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  },
+  props: {
+    isOutsideIndicator: {
+      type: Boolean,
+      default: false
+    },
+    isCard: {
+      type: Boolean,
+      default: false
+    },
+    isVertical: {
+      type: Boolean,
+      default: false
+    },
+    carouselShowArrow: {
+      type: String,
+      default: 'hover'
+    }
+  }
+}
+</script>
