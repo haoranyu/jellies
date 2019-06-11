8e9df1909212d0f335f8dd232647e494a72c762d
Haoran Yu <yuhaoran@jisuanke.com>
2019-06-11 18:05:14 +0800

    Add Timeline as a component. #46

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-timeline-item
+    v-bind="trimAttrs($attrs)"
+    :hide-timestamp="isTimeHidden"
+    :timestamp="timelineItemTime"
+  >
+    <slot></slot>
+  </el-timeline-item>
+</template>
+
+<script>
+export default {
+  name: 'JskTimelineItem',
+  inheritAttrs: false,
+  props: {
+    isTimeHidden: {
+      type: Boolean,
+      default: false
+    },
+    timelineItemTime: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'timeline-item-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  }
+}
+</script>
