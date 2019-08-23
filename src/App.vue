eb97c0e1c4845d97bc0416c859923c926d80e83e
Haoran Yu <haoran_yu@hotmail.com>
2019-08-23 11:28:04 +0800

    Add side-menu component. Close #56

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
-  <div>
-    <el-button @click="drawerVisible = true" type="primary" style="margin-left: 16px;">
-      点我打开
-    </el-button>
-    <jsk-drawer
-      :visible.sync="drawerVisible"
-      drawer-position="left">
-      <span>我来啦!</span>
-    </jsk-drawer>
-
-  </div>
+  <jsk-side-menu
+    :is-collapsed.sync="isCollapsed"
+    :has-collapse="true"
+    side-menu-min-height="400px"
+    side-menu-default-active="2"
+    is-fullscreen
+    @open="handleOpen"
+    @close="handleClose">
+    <jsk-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <jsk-menu-item-group>
+        <template slot="title">分组一</template>
+        <jsk-menu-item index="1-1">选项1</jsk-menu-item>
+        <jsk-menu-item index="1-2">选项2</jsk-menu-item>
+      </jsk-menu-item-group>
+      <jsk-menu-item-group title="分组2">
+        <jsk-menu-item index="1-3">选项3</jsk-menu-item>
+      </jsk-menu-item-group>
+      <jsk-submenu index="1-4">
+        <template slot="title">选项4</template>
+        <jsk-menu-item index="1-4-1">选项1</jsk-menu-item>
+      </jsk-submenu>
+    </jsk-submenu>
+    <jsk-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">导航二</span>
+    </jsk-menu-item>
+    <jsk-menu-item index="3" disabled>
+      <i class="el-icon-document"></i>
+      <span slot="title">导航三</span>
+    </jsk-menu-item>
+    <jsk-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <span slot="title">导航四</span>
+    </jsk-menu-item>
+  </jsk-side-menu>
 </template>
 
 <script>
-import JskDrawer from './components/Drawer.vue'
+import JskSideMenu from './components/SideMenu.vue'
+import JskMenuItemGroup from './components/MenuItemGroup.vue'
+import JskMenuItem from './components/MenuItem.vue'
+import JskSubmenu from './components/Submenu.vue'
 export default {
   data() {
     return {
-      drawerVisible: false
+      isCollapsed: false
     };
   },
   methods: {
-    test: function() {
-      alert('xxx');
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   },
   components: {
-    JskDrawer
+    JskSideMenu,
+    JskMenuItemGroup,
+    JskMenuItem,
+    JskSubmenu
   }
 };
 </script>
---
+++ src/components/MenuItemGroup.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu-item-group
+    v-bind="trimAttrs($attrs)"
+  >
+    <template slot="title">
+      <slot name="title"></slot>
+    </template>
+    <slot></slot>
+  </el-menu-item-group>
+</template>
+
+<script>
+export default {
+  name: 'JskMenuItemGroup',
+  inheritAttrs: false,
+  methods: {
+    trimAttrs: function(attrs) {
+      Object.keys(attrs).forEach((key) => {
+        let prefixs = ['is-', 'has-', 'menu-item-group-'];
+        prefixs.forEach((prefix) => {
+          if (key.substr(0, prefix.length) === prefix) {
+            attrs[key.substr(prefix.length)] = attrs[key];
+          }
+        })
+      })
+      return attrs;
+    }
+  }
+}
+</script>
