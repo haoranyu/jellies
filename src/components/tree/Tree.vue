edddce6eb3d8a4a7adab1b7e4cc8c166d3ae8e06
Haoran Yu <haoran_yu@hotmail.com>
2023-05-09 00:04:36 +0800

    Refactor: Better syntax of tree component. Ref #25

--- src/components/tree/Tree.vue
+++ src/components/tree/Tree.vue
@@ -7,21 +7,15 @@ TreeWrapper.components.ElTreeNode.components.NodeContent.render = function(h) {
   const node = this.node;
   const { data, store } = node;
 
-  return parent.renderContent ? (
-    parent.renderContent.call(parent._renderProxy, h, {
+  return parent.renderContent ? h(parent.renderContent.call(parent._renderProxy, h, {
       _self: tree.$vnode.context,
       node,
       data,
       store
-    })
-  ) : tree.$scopedSlots.default ? (
-    tree.$scopedSlots.default({ node, data })
-  ) : (
-    <span class="el-tree-node__label">
-      <i class={data.icon ? [data.icon, 'el-tree-node__icon'] : ''}></i>
-      {data.label}
-    </span>
-  );
+    })) : tree.$scopedSlots.default ? tree.$scopedSlots.default({ node, data }) : h('span', { class: 'el-tree-node__label' }, [
+      data.icon ? h('i', { class: [data.icon, 'el-tree-node__icon'] }) : '',
+      data.label
+    ]);
 };
 TreeWrapper.name = 'JskTree';
 export default TreeWrapper;
