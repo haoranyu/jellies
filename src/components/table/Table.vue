1a02bc89abf2241c3dc8945bf55941e307ac2cb3
Weimeng <weimeng@jisuanke.com>
2025-01-10 15:25:47 +0800

    Feature: Better setting on table component. Ref #207

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,55 +1,37 @@
 <template>
-  <div
-    :class="[{
-      'jsk-table--has-top-border': hasTopBorder
-    }, $attrs.class]"
-    :style="$attrs.style"
+  <el-table
     v-bind="$attrs"
+    v-on="$listeners"
+    :class="{ 'jsk-table--has-top-border': hasTopBorder }"
   >
-    <el-table v-bind="$attrs" v-on="$listeners">
-      <slot />
-    </el-table>
-  </div>
+    <slot />
+  </el-table>
 </template>
 
 <script>
-import { Table } from 'element-ui';
-
+import { Table } from 'element-ui'
 export default {
   name: 'JskTable',
-  components: {
-    ElTable: Table
-  },
   inheritAttrs: false,
   props: {
     hasTopBorder: {
       type: Boolean,
       default: true
     }
+  },
+  components: {
+    ElTable: Table
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
 @import '../../plugins/theme-variables.scss';
-
-.jsk-table--has-top-border {
-  ::v-deep .el-table {
-    border-top: 2px solid #ddd !important;
-    &::before {
-      height: 2px !important;
-      display: block !important;
-      z-index: 5;
-    }
-  }
-}
-
-::v-deep .el-table {
+.el-table {
   border-top: none;
   &::before {
     display: none;
   }
-  
   &.el-table--border {
     border-top: 1px solid #ddd;
     &::before {
@@ -57,9 +39,18 @@ export default {
       display: block;
     }
   }
-  
-  .cell {
-    line-height: 38px;
+  ::v-deep {
+    .cell {
+      line-height: 38px;
+    }
+  }
+  &.jsk-table--has-top-border {
+    border-top: 2px solid #ddd !important;
+    &::before {
+      height: 2px !important;
+      display: block !important;
+      z-index: 5;
+    }
   }
 }
 </style>
